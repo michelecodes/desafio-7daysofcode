f33d8dfa9192d07f52052bb29714da27afd7814c
Michele Ribeiro <133832334+michelecodes@users.noreply.github.com>
2024-02-10 12:55:17 -0300

    Create dia1-resolucao.js

---
+++ dia1-resolucao.js
@@ -0,0 +1,24 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (numeroUm == stringUm) {
+  console.log('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não têm o mesmo valor ou tipo')
+}
+
+if (numeroTrinta == stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta têm o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não têm o mesmo valor ou tipo')
+}
+
+if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez têm o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não têm o mesmo valor ou tipo')
+}
